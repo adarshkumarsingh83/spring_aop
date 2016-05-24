b7221c58c8d39d80b6e19d75b51fbe947f8b6261
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2016-05-24 19:34:06 +0530

    SpringAopXmlExample

---
+++ SpringAopXmlExample/src/main/java/com/espark/adarsh/aop/EmployeeServiceAspect.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2015 Espark And ©Adarsh Development Services @copyright All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Espark nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.espark.adarsh.aop;
+
+import com.espark.adarsh.bean.Employee;
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+
+
+/**
+ * @author Adarsh Kumar
+ * @author $LastChangedBy: Adarsh Kumar$
+ * @version $Revision: 0001 $, $Date:: 1/1/10 0:00 AM#$
+ * @Espark @copyright all right reserve
+ */
+public class EmployeeServiceAspect {
+
+    private static final Logger LOGGER= Logger.getLogger(EmployeeServiceAspect.class);
+
+    public void employeeServiceSaveEmployeeBeforeAdvice(JoinPoint joinPoint){
+        Employee employee=(Employee)(joinPoint.getArgs())[0];
+        LOGGER.info("Before executing employee service save method "+employee);
+    }
+
+    public Object employeeServiceSaveEmployeeAroundAdvice(ProceedingJoinPoint joinPoint)throws Throwable{
+        Employee employee=(Employee)(joinPoint.getArgs())[0];
+        LOGGER.info("Before executing employee service save method "+employee);
+        Object result=joinPoint.proceed();
+        LOGGER.info("After executing employee service save method ");
+        return result;
+    }
+
+    public void employeeServiceSaveEmployeeAfterAdvice(JoinPoint joinPoint){
+        Employee employee=(Employee)(joinPoint.getArgs())[0];
+        LOGGER.info("Before executing employee service save method "+employee);
+    }
+
+    public void employeeServiceSaveEmployeeAfterReturningAdvice(JoinPoint joinPoint,Object result){
+        LOGGER.info("Exception Generated During executing employee service save method "+result);
+    }
+
+    public void employeeServiceSaveEmployeeAfterThrowingAdvice(JoinPoint joinPoint,Throwable  error){
+        LOGGER.info("After Returning executing employee service save method "+error);
+    }
+
+}
---
+++ SpringAopXmlExample/src/main/java/com/espark/adarsh/bean/Employee.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2015 Espark And ©Adarsh Development Services @copyright All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Espark nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.espark.adarsh.bean;
+
+/**
+ * @author Adarsh Kumar
+ * @author $LastChangedBy: Adarsh Kumar$
+ * @version $Revision: 0001 $, $Date:: 1/1/10 0:00 AM#$
+ * @Espark @copyright all right reserve
+ */
+public class Employee {
+
+    private Integer employeeId;
+    private String employeeName;
+    private String employeeEmail;
+
+    public Employee(){
+        super();
+    }
+
+    public Employee(Integer employeeId, String employeeName, String employeeEmail) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.employeeEmail = employeeEmail;
+    }
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeEmail() {
+        return employeeEmail;
+    }
+
+    public void setEmployeeEmail(String employeeEmail) {
+        this.employeeEmail = employeeEmail;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", employeeName='" + employeeName + '\'' +
+                ", employeeEmail='" + employeeEmail + '\'' +
+                '}';
+    }
+}
